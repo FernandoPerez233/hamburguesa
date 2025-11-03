77dc96f214b9b95c065fa7bf150f98ff7d792823
FernandoPerez233 <al45722@un.edu.mx>
2025-11-03 09:46:29 -0600

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+var formulario = document.getElementById("cantidad");
+
+function Calcular{
+    // Código para la función Calcular
+
+    // alert("Función Calcular ejecutada"); //
+
+    alert(formulario);
+}
